d89438cd81b42c4cb5eca6a9bbc886cf5a80e51e
Andreassen <spencer.andreassen@PIONEER.COM>
2017-11-15 20:57:58 -0600

    a little more progress

--- schema.js
+++ schema.js
@@ -6,29 +6,20 @@ var { GraphQLObjectType, GraphQLNonNull, GraphQLInt, GraphQLString } = require('
 var product = require('./schema/product');
 var cart = require('./schema/cart');
 
+// cart fields must be an object with field names as keys or a function which returns such an object
+// CartQueryType.id field config must be an object
 var schema = new GraphQLSchema({
     query: new GraphQLObjectType({
-        name: 'CartQueryType',
-        fields: {
-            cart: {
-                type: new GraphQLObjectType(cart),
-                args: {
-                    id: {
-                        name: 'id',
-                        type: new GraphQLNonNull(GraphQLInt)
-                    }
-                },
-                resolve: (cart, { id }, source) => {
-                    return { 
-                        id: 1,
-                        lineItems: [{
-                            id: 1,
-                            productId: 2,
-                            quantity: 5
-                        }]
-                    };
-                }
-            }
+        name: 'cart',
+        fields: () => {
+            return {
+                id: 1,
+                lineItems: [{
+                    id: 1,
+                    productId: 2,
+                    quantity: 5
+                }]
+            };
         }
     })
 });
