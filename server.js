6548193de58fbb17950d385b1aca745f5283ee76
Andreassen <spencer.andreassen@PIONEER.COM>
2017-11-19 18:48:49 -0600

    adding option to server.js

--- server.js
+++ server.js
@@ -3,14 +3,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
+const cors = require('cors');
 
 const schema = require('./schema');
 
-const port = 3000;
+const port = 9000;
 let app = express();
-app.use('/', graphqlHTTP({
+
+app.use('/', cors(), graphqlHTTP({
   schema: schema,
   graphiql: true,
+  pretty: true
 }));
 app.listen(port);
 console.log(`Running a GraphQL API server at localhost:${port}/`);
